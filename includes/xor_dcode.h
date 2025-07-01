ec3d232202131ba2ef5e7dd8229d50ac7b8c400d
Moac <benjamsc@student.42.fr>
2025-07-01 16:45:08 +0200

    first part finished, focus on the thread party that sigsev

--- includes/xor_dcode.h
+++ includes/xor_dcode.h
@@ -1,55 +1,23 @@
 #ifndef XOR_DECODE_H
 # define XOR_DECODE_H
 
-# include "libft.h"
-# include <pthread.h>
+#include "xor_def.h"
 
-# define ONE_LINE 0
-# define ZEROX 1
-# define BSX 2			// backslashx //x
-# define SPACE 3
-# define COLUMN 4
-# define SEMI_COL 5
-# define COMMA 6
-
-#define CHAR_MIN_BY_THREADS 1024
-
-
-/*typedef struct s_list*/
-/*{*/
-/*	void				*content;*/
-/*	struct s_list		*next;*/
-/*}						t_list;*/
-
-typedef struct s_data
-{
-	t_uint		max_threads;
-	t_ulong		char_by_thread;
-	t_uint		nb_threads;
-	t_list		**head;
-}				t_data;
-
-typedef struct s_data_node
-{
-	char			*trinome;
-	int				count;
-	int				*pos;
-	pthread_mutex_t	*mtx_node;
-} t_data_node;
-
-typedef struct s_reader
-{
-	char	*sample;
-	t_ulong	length;
-	t_list	**h_list;
-} t_reader;
-
-// format HEX
+//	hex_parsing.c
 char	*remove_whitespace(char *str);
 char	*reset_hex(char *str);
+char	*ft_str_insert(char *str, char *to_insert, int step);
 char	*hex_format(char *str, int type);
-// multi-threading
-char	**split_for_threads(char *str, t_data *data);
-
+//	multi-threading
+// char	**split_for_threads(t_data *data);
+int		create_threads(t_data *data);
+//	data_node_utils.c
+int		add_data_node(t_list **head, char *str, int pos);
+int		update_data_node(t_data_node *data, int pos);
+void    *extract_data_node(void *ptr_data, char type);
+void    free_data_node(void *data);
+//	list_utils.c
+void	print_list(t_list **head);
+void	destroy_list(t_list **head);
 
 #endif
---
+++ srcs/data_node_utils.c
@@ -0,0 +1,85 @@
+
+#include "xor_dcode.h"
+
+int	add_data_node(t_list **head, char *str, int pos)
+{
+	t_data_node	*data;
+	t_list		*new;
+
+	ft_fprintf(2, "%sDEBUG tu rentre bien\n", YELLOW, RESET);
+	data = ft_calloc(1, sizeof(t_data_node));
+	if (!data)
+		return (0);
+	data->trigram = ft_strndup(str, 3);
+	if (!data->trigram)
+		return (free_data_node(data), 0);
+	data->count = 1;
+	data->pos = ft_calloc(1, sizeof(int));
+	if (!data->pos)
+		return (free_data_node(data), 0);
+	data->pos[0] = pos;
+	data->mtx_node = ft_calloc(1, sizeof(t_mtx));
+	if (!data->mtx_node)
+		return (free_data_node(data), 0);
+	if (pthread_mutex_init(data->mtx_node, NULL) != 0)
+		return (free(data->mtx_node), free_data_node(data), 0);
+	new = ft_lstnew(data);
+	if (!new)
+		free_data_node(data);
+	ft_lstadd_back(head, new);
+	ft_fprintf(2, "%sDEBUG tu resort pas\n", YELLOW, RESET);
+	return (1);
+}
+
+int	update_data_node(t_data_node *data, int pos)
+{
+	int	*new_pos;
+	int	i;
+
+	data->count++;
+	new_pos = ft_calloc((data->count + 1), sizeof(int));
+	if (!new_pos)
+		return (0);
+	i = -1;
+	while (++i < (data->count - 1))
+		new_pos[i] = data->pos[i];
+	new_pos[i] = pos;
+	free(data->pos);
+	data->pos = new_pos;
+	return (1);
+}
+
+void	*extract_data_node(void *ptr_data, char type)
+{
+	t_data_node	*data;
+
+	data = (t_data_node *)ptr_data;
+	if (type == TRIGRAM)
+		return (data->trigram);
+	if (type == POS)
+		return (data->pos);
+	if (type == MTX)
+		return (data->mtx_node);
+	if (type == COUNT)
+		return (&data->count);
+	return (NULL);
+}
+
+void	free_data_node(void *ptr_data)
+{
+	t_data_node	*data;
+
+	data = (t_data_node *)ptr_data;
+	if (!data)
+		return ;
+	if (data->trigram)
+		free(data->trigram);
+	if (data->pos)
+		free(data->pos);
+	if (data->mtx_node)
+	{
+		pthread_mutex_destroy(data->mtx_node);
+		free(data->mtx_node);
+	}
+	free(data);
+}
